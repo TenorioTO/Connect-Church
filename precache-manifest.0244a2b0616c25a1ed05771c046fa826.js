00a8323fc725619c153736f502f13fa81e4195e2
Thiago Oliveira <thiagooliveira@Thiagos-MacBook-Air.local>
2020-08-03 20:27:12 -0300

    Updates

---
+++ precache-manifest.0244a2b0616c25a1ed05771c046fa826.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4a337449ca16a397ccd4932891d72f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fab92a81fe334e74d09a",
+    "url": "/static/css/main.04baaca9.chunk.css"
+  },
+  {
+    "revision": "736817902f66c36ffb54",
+    "url": "/static/js/2.f5f861aa.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.f5f861aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab92a81fe334e74d09a",
+    "url": "/static/js/main.6ab6c5d3.chunk.js"
+  },
+  {
+    "revision": "e5cfcd17c84a3658b8c6",
+    "url": "/static/js/runtime-main.af5043da.js"
+  },
+  {
+    "revision": "bee1ddf907edcac61c560289021c7027",
+    "url": "/static/media/background_whitelogo.bee1ddf9.svg"
+  },
+  {
+    "revision": "5f812ec17de4cc2e9c1de546d651e87b",
+    "url": "/static/media/banner_background.5f812ec1.jpg"
+  },
+  {
+    "revision": "56a4654c004ef63897cde35c0c1e5989",
+    "url": "/static/media/descomplicando.56a4654c.jpeg"
+  },
+  {
+    "revision": "02405554e7e9889a4e2158284f5af864",
+    "url": "/static/media/hello.02405554.jpeg"
+  },
+  {
+    "revision": "b091ae2721f180a4c5b16b72cf877269",
+    "url": "/static/media/mission_show.b091ae27.jpeg"
+  },
+  {
+    "revision": "e7f2b3cf326799cdb1ceb21c3fbcc74a",
+    "url": "/static/media/podcast.e7f2b3cf.jpg"
+  },
+  {
+    "revision": "fe2f84552b0d8e43b4368b2d5759ded8",
+    "url": "/static/media/site.fe2f8455.png"
+  },
+  {
+    "revision": "2b475263c8a8e195fe5d439c033e3c29",
+    "url": "/static/media/site2.2b475263.png"
+  },
+  {
+    "revision": "9aaf318167baaeb1f0fb10d757e3d8d7",
+    "url": "/static/media/site3.9aaf3181.png"
+  }
+]);
